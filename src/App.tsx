e35f0b2b2e90f4608f1517397b12d7ade6fe7097
gandroide <ajontiveros82@gmail.com>
2023-12-12 20:50:05 +0000

    test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import react, { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import PrimaryButton from "./components/button/PrimaryButton"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <PrimaryButton title="hola" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
