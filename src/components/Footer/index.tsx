04daf96c9b73d4fbf59dcaeb551786177758af1a
luisfilipemendonca <filipedev@outlook.pt>
2024-02-17 20:28:18 +0300

    Adicionado componente footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Electroflow &#169; {new Date().getFullYear()} - Todos os direitos reservados
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./Utils"
 import { I18nextProvider } from "react-i18next"
 import i18n from "./locales/translations"
+import Footer from "./components/Footer"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <react.StrictMode>
     <I18nextProvider i18n={i18n}>
       <RouterProvider router={router} />
+      <Footer />
     </I18nextProvider>
   </react.StrictMode>,
 )
