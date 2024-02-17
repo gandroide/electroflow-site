9a916cbb2270c5cb011cc63b04b81b3d9ac87b5b
gandroide <ajontiveros82@gmail.com>
2024-02-17 16:57:18 +0000

    second commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
 import react, { FC } from "react"
-import Header from "../components/header/Header"
 import SwiperComponent from "../components/swiper/Swiper"
+import Card from "../components/card/Card"
 
 const Home: FC = () => {
-  return <SwiperComponent />
+  return (
+    <>
+      <SwiperComponent />
+      <div className="flex h-screen">
+        <Card
+          title="Quem somos"
+          subtitle="Destacamo-nos como uma equipa jovem e dinâmica com amplos conhecimentos da indústria em questão, na generalidade com mais de 15 anos de experiência do ramo, e sensíveis às necessidades dos nossos Clientes."
+          buttonText="Saiba mais"
+        />
+        <Card title="Oferta integrada " />
+        <Card title="Áreas de intervenção" />
+      </div>
+      <div className="h-screen">cosas de la empresa! fotos y logros!</div>
+    </>
+  )
 }
 
 export default Home
