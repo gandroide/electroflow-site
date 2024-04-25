4d74689d466ab7cc911b4964a3293ced40a270dd
luisfilipemendonca <filipedev@outlook.pt>
2024-04-25 02:11:38 -0700

    Correção na sectiion da missão

--- src/layouts/Misson/index.jsx
+++ src/layouts/Misson/index.jsx
@@ -1,30 +1,41 @@
 import react from 'react';
-import { StyledMissonSection, StyledMissonContainer } from "./styled";
+
+import { Grid, GridItem } from '../../components/Grid'; 
+
+import { StyledMissonSection, StyledMissonContainer, StyledMissonText, StyledMissonSeparator } from "./styled";
 import { StyledSecondaryTitle, StyledSecondaryTitleEmphasis } from '../../styles';
 
 const Misson = () => {
     return (
         <StyledMissonSection>
-            <StyledMissonContainer>
-                <StyledSecondaryTitle>
-                    A nossa <StyledSecondaryTitleEmphasis>missão</StyledSecondaryTitleEmphasis>
-                </StyledSecondaryTitle>
-                <p>
-                    A Electroflow tem como principal missão satisfazer as necessidades
-                    dos seus clientes, desenvolvendo e implementando soluções de
-                    engenharia integradas, com recurso a tecnologias eficientes e
-                    sustentáveis.
-                </p>
-            </StyledMissonContainer>
-
-            <StyledMissonContainer>
-                <StyledSecondaryTitle>A nossa <StyledSecondaryTitleEmphasis>visão</StyledSecondaryTitleEmphasis></StyledSecondaryTitle>
-                <p>
-                    Pretendemos destacar-nos como referência em oferta integrada de
-                    projeto, instalação e manutenção das áreas de negócio em que estamos
-                    envolvidos, com especial enfoque no mercado regional.
-                </p>
-            </StyledMissonContainer>
+            <Grid>
+                <GridItem columns={{ lg: 5 }}>
+                    <StyledMissonContainer>
+                        <StyledSecondaryTitle>
+                            A nossa <StyledSecondaryTitleEmphasis>missão</StyledSecondaryTitleEmphasis>
+                        </StyledSecondaryTitle>
+                        <StyledMissonText>
+                            A Electroflow tem como principal missão satisfazer as necessidades
+                            dos seus clientes, desenvolvendo e implementando soluções de
+                            engenharia integradas, com recurso a tecnologias eficientes e
+                            sustentáveis.
+                        </StyledMissonText>
+                    </StyledMissonContainer>
+                </GridItem>
+                <GridItem columns={{ lg: 2 }}>
+                    <StyledMissonSeparator />
+                </GridItem>
+                <GridItem columns={{ lg: 5 }}>
+                    <StyledMissonContainer>
+                        <StyledSecondaryTitle>A nossa <StyledSecondaryTitleEmphasis>visão</StyledSecondaryTitleEmphasis></StyledSecondaryTitle>
+                        <StyledMissonText>
+                            Pretendemos destacar-nos como referência em oferta integrada de
+                            projeto, instalação e manutenção das áreas de negócio em que estamos
+                            envolvidos, com especial enfoque no mercado regional.
+                        </StyledMissonText>
+                    </StyledMissonContainer>
+                </GridItem>
+            </Grid>
         </StyledMissonSection>
     )
 }
--- src/layouts/Misson/styled.js
+++ src/layouts/Misson/styled.js
@@ -1,12 +1,33 @@
 import styled from 'styled-components';
-import { StyledSection } from '../../styles'
+import { StyledSection } from '../../styles';
+
+export const StyledMissonContainer = styled.div``;
 
 export const StyledMissonSection = styled(StyledSection)`
     min-height: unset;
 `;
 
-export const StyledMissonContainer = styled.div`
-    &:not(:last-child) {
-        margin-bottom: 100px;
+export const StyledMissonText = styled.p`
+    text-align: center;
+
+    @media screen and (min-width: 992px) {
+        font-size: 18px;
     }
-`
+
+    @media screen and (min-width: 1200px) {
+        font-size: 20px;
+    }
+`;
+
+export const StyledMissonSeparator = styled.div`
+    width: 50%;
+    height: 2px;
+    background-color: red;
+    margin: 75px auto;
+
+    @media screen and (min-width: 992px) {
+        width: 2px;
+        height: 100%;
+        margin: 0 auto;
+    }
+`;
