0e2e32ab470f5a83e1a263441bb10951b201212e
gandroide <ajontiveros82@gmail.com>
2024-05-17 07:30:24 +0100

    ok 3

--- src/layouts/Misson/styled.ts
+++ src/layouts/Misson/styled.ts
@@ -2,49 +2,49 @@ import styled from 'styled-components';
 import { StyledSection } from '../../styles';
 
 export const StyledMissonContainer = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    opacity: 0;
-    transform: translateY(100px);
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  /* opacity: 0; */
+  /* transform: translateY(100px); */
 `;
 
 export const StyledMissonSection = styled(StyledSection)`
-    min-height: unset;
-    overflow: hidden;
+  min-height: unset;
+  overflow: hidden;
 `;
 
 export const StyledMissonText = styled.p`
-    text-align: center;
-    color: ${({theme}) => theme.palette.thunder};
+  text-align: center;
+  color: ${({ theme }) => theme.palette.thunder};
 
-    @media screen and (min-width: 992px) {
-        font-size: 18px;
-    }
+  @media screen and (min-width: 992px) {
+    font-size: 18px;
+  }
 
-    @media screen and (min-width: 1200px) {
-        font-size: 20px;
-    }
+  @media screen and (min-width: 1200px) {
+    font-size: 20px;
+  }
 `;
 
 export const StyledMissonSeparator = styled.div`
-    width: 50%;
-    height: 2px;
-    background-color: ${({theme}) => theme.palette.boston};
-    margin: 75px auto;
+  width: 50%;
+  height: 2px;
+  background-color: ${({ theme }) => theme.palette.boston};
+  margin: 75px auto;
 
-    @media screen and (min-width: 992px) {
-        width: 3px;
-        height: 100%;
-        margin: 0 auto;
-    }
+  @media screen and (min-width: 992px) {
+    width: 3px;
+    height: 100%;
+    margin: 0 auto;
+  }
 `;
 
 export const StyledMissonIcon = styled.span`
-    margin-bottom: 64px;
+  margin-bottom: 64px;
 
-    & svg {
-        font-size: 128px;
-        fill: ${({theme}) => theme.palette.boston};
-    }
+  & svg {
+    font-size: 128px;
+    fill: ${({ theme }) => theme.palette.boston};
+  }
 `;
