c0ed3cdd04ab71cdbf3c9c0e457da20552ae6129
gandroide <ajontiveros82@gmail.com>
2024-10-15 11:35:01 +0100

    fix translations error

--- src/layouts/Products/index.tsx
+++ src/layouts/Products/index.tsx
@@ -1,4 +1,4 @@
-import react, { FC, useState } from 'react';
+import react, { FC, useState, useTransition } from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
@@ -44,6 +44,7 @@ const ProductCard: FC<ProductCardProps> = ({
   image,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const { t } = useTranslation();
 
   const onOpenModalHandler = () => {
     setIsOpen(true);
@@ -61,7 +62,7 @@ const ProductCard: FC<ProductCardProps> = ({
             <StyledProductDescription>{carousel}</StyledProductDescription>
           </StyledProductDescriptionContainer>
           <StyledProductLink onClick={onOpenModalHandler}>
-            Saber mais
+            {t('translations.products.moreInfo')}
           </StyledProductLink>
         </StyledProductContent>
       </StyledProductContainer>
@@ -92,22 +93,14 @@ const Products = () => {
       id: 2,
       title: t('translations.products.1.title'),
       carousel: t('translations.products.1.description'),
-      modal: [
-        `Como quadristas certificados pela HAGER, a ELECTROFLOW projeta, fabrica e monta quadros elétricos, tanto de potência como de comando, de acordo com as especificações do cliente, e utilizando componentes dos mais prestigiados fabricantes.`,
-        `Um quadro elétrico, ou de distribuição, é um equipamento elétrico destinado a receber energia elétrica de uma ou mais fontes de alimentação e distribui-las a um ou mais circuitos. Destinado a abrigar um ou mais dispositivos de proteção e/ou manobra e a conexão de condutores elétricos interligados a eles, a fim de distribuir a energia elétrica aos diversos circuitos.`,
-        `Em qualquer instalação elétrica, devemos saber como realizar os procedimentos correspondentes à execução de um projeto elaborado previamente, em conformidade com as especificações previstas na NBR 5410 (norma que regulamenta as instalações elétricas em baixa tensão) e NR-10 (segurança em instalações e serviços em eletricidade).`,
-      ],
+      modal: [t('translations.products.1.modal.0')],
       image: Quadros,
     },
     {
       id: 3,
       title: t('translations.products.2.title'),
       carousel: t('translations.products.2.description'),
-      modal: [
-        `Nesta gama de produtos somos distribuidores exclusivos da SEW.`,
-        `O sistema modular de motorredutores SEW está orientado para uma panóplia de áreas de aplicação. Seleccione o accionamento ideal para si, entre os motorredutores standard, de sistema de suporte suspenso elétrico, servomotoredutores, motoredutores de ajuste, em aço inoxidável ou à prova de explosão.`,
-        `A gama de funções compacta, o manuseamento simples e uma integração rápida na aplicação são características que distinguem os conversores de frequência MOVITRAC® LTE-B. Estão disponíveis em 3 tamanhos na gama de potências de 0,37 kW até 7,5 kW para as tensões de alimentação de 115 V, 230 V e 400 V. Concebido e desenvolvido para o controlo da velocidade de motores assíncronos sem encoder, o conversor realiza de forma fiável as suas aplicações de transporte simples. Especialmente em pequenos tranportadores modulares, em ventiladores e bombas.`,
-      ],
+      modal: [t('translations.products.2.modal.1')],
       image: Motoredutores,
     },
     {
@@ -115,8 +108,8 @@ const Products = () => {
       title: t('translations.products.3.title'),
       carousel: t('translations.products.3.description'),
       modal: [
-        `Fornecemos todo o tipo de bombas para abastecimento doméstico, industrial, piscina, rega ou qualquer outro tipo de situação. A escolha do tipo de bomba de água ou de outro fluído requer conhecer o caudal e a pressão pretendida no ponto mais desfavorável, seja para abastecimento doméstico, industrial, piscina, rega ou qualquer outro tipo de situação.  Como principais parceiros temos as Marcas CAPRARI e a EBARA.`,
-        `Para obter uma cotação para o seu caso, contacte-nos com a seguinte informação:`,
+        t('translations.products.3.modal.0'),
+        t('translations.products.3.modal.1'),
       ],
       image: Bombas,
     },
@@ -125,8 +118,8 @@ const Products = () => {
       title: t('translations.products.4.title'),
       carousel: t('translations.products.4.description'),
       modal: [
-        `Nesta área de negócio somos distribuidores e de assistência técnica aprovados das Marcas DAIKIN, SAMSUNG e TOYOTOMI.`,
-        `O aquecimento, ventilação e ar condicionado (AVAC) refere-se a uma gama de equipamentos que tem como finalidade climatizar diversos ambientes para, assim, garantir o conforto térmico desses espaços. Nesse sentido, além de proporcionar uma temperatura agradável, o sistema AVAC é responsável por filtrar o ar e eliminar bactérias, mofos e fungos do ambiente em que está inserido. Os equipamentos de AVAC também podem vir integrados com sistema de gestão e poupança energética.`,
+        t('translations.products.4.modal.0'),
+        t('translations.products.4.modal.1'),
       ],
       image: Avac,
     },
@@ -135,8 +128,8 @@ const Products = () => {
       title: t('translations.products.5.title'),
       carousel: t('translations.products.5.description'),
       modal: [
-        `A energia solar fotovoltaica é a energia obtida através da conversão direta da luz em eletricidade por meio do efeito fotovoltaico. A célula fotovoltaica, um dispositivo fabricado com material semicondutor, é a unidade fundamental desse processo de conversão.`,
-        `Este tipo de energia usa-se para alimentar uma grande variedade de aplicativos e aparelhos autónomos, para abastecer refúgios ou moradias isoladas da rede elétrica e para produzir eletricidade a grande escala através de redes de distribuição. Devido à crescente demanda de energias renováveis, a fabricação de células solares e instalações fotovoltaicas tem avançado consideravelmente nos últimos anos.`,
+        t('translations.products.5.modal.0'),
+        t('translations.products.5.modal.1'),
       ],
       image: Paineis,
     },
